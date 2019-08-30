1bb99dd02689b24ea67c76f4eef27a349a34db68
Raweewat <bookboks@hotmail.com>
2019-08-30 22:49:15 +0700

    start

---
+++ Assets/Script/BossController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    private int currentBossHP;
+    private int maxBossHP;
+    [SerializeField] private GameObject BossHPBarUI;
+    private bool isBossAttack;
+    private float bossCooldownAttackCounter;
+    public float bossCooldownAttackTime;
+    private bool isFaceLeft;
+    [SerializeField] private PlayerController thePlayer1;
+    [SerializeField] private PlayerController thePlayer2;
+    [SerializeField] private GameObject BossAvatar1;
+    [SerializeField] private GameObject BossAvatar2;
+
+    void Start()
+    {
+        maxBossHP = 20;
+        currentBossHP = maxBossHP;
+        isBossAttack = false;
+        isFaceLeft = true;
+    }
+
+    void Update()
+    {
+
+        if (bossCooldownAttackCounter >= 0)
+            bossCooldownAttackCounter -= Time.deltaTime;
+        else if (bossCooldownAttackCounter < 0)
+        {
+            isBossAttack = true;
+            if (isFaceLeft)
+                thePlayer1.DecreasHP(1);
+            else
+                thePlayer2.DecreasHP(1);
+            bossCooldownAttackCounter = bossCooldownAttackTime;
+        }
+    }
+
+    public void AttackBoss(string playerName)
+    {
+        if (playerName.Equals("Player1"))
+        {
+            isFaceLeft = true;
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+        }
+        else if (playerName.Equals("Player2"))
+        {
+            isFaceLeft = false;
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        }
+        currentBossHP -= 1;
+        UpdateBossHPBar();
+        if (currentBossHP == 0)
+            BossDeath();
+    }
+
+    void UpdateBossHPBar()
+    {
+        float hp_ratio = (float)currentBossHP / (float)maxBossHP;
+        if (hp_ratio >= 0)
+            BossHPBarUI.transform.localScale = new Vector3(hp_ratio, 1, 1);
+    }
+
+    void BossDeath()
+    {
+        Debug.Log("BossDead");
+    }
+
+
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool isPlayer1;
+    public bool isPlayer2;
+    private float cooldownAttackCounter;
+    public float cooldownAttackTime;
+    private bool isP1Attack;
+    private bool isP2Attack;
+    private int currentHP;
+    private int maxHP;
+    [SerializeField] private GameObject HPBarUI;
+    [SerializeField] private BossController theBoss;
+
+    void Start()
+    {
+        maxHP = 10;
+        currentHP = maxHP;
+    }
+
+    void Update()
+    {
+        #region   //Controller
+        if (Input.anyKey)
+        {
+            if (isPlayer1)
+                if (Input.GetButtonDown("P1Attack") && !isP1Attack)
+                {
+                    isP1Attack = true;
+                    cooldownAttackCounter = cooldownAttackTime;
+                    theBoss.AttackBoss("Player1");
+                }
+            if (isPlayer2)
+                if (Input.GetButtonDown("P2Attack") && !isP2Attack)
+                {
+                    isP2Attack = true;
+                    cooldownAttackCounter = cooldownAttackTime;
+                    theBoss.AttackBoss("Player2");
+                }
+        }
+        #endregion
+
+        if (isPlayer1)
+            if (isP1Attack)
+                if (cooldownAttackCounter > 0)
+                    cooldownAttackCounter -= Time.deltaTime;
+                else if (cooldownAttackCounter <= 0)
+                    isP1Attack = false;
+
+        if (isP2Attack)
+            if (isP2Attack)
+                if (cooldownAttackCounter > 0)
+                    cooldownAttackCounter -= Time.deltaTime;
+                else if (cooldownAttackCounter <= 0)
+                    isP2Attack = false;
+    }
+
+    void UpdatePlayerHPBar()
+    {
+        float hp_ratio = (float)currentHP / (float)maxHP;
+        if (hp_ratio <= 1)
+            HPBarUI.transform.localScale = new Vector3(hp_ratio, 1, 1);
+    }
+
+    public void DecreasHP(int damage)
+    {
+        currentHP -= 1;
+        UpdatePlayerHPBar();
+    }
+}
