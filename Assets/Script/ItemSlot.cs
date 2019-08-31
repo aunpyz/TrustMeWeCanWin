69f5f34e7f1589d5cea0d785b454c2f70e6bbe32
Aunpyz <aunnop_kattiyanet@hotmail.com>
2019-08-31 22:55:27 +0700

    Display items' icon

--- Assets/Script/ItemSlot.cs
+++ Assets/Script/ItemSlot.cs
@@ -7,27 +7,31 @@ public class ItemSlot : MonoBehaviour
 {
     public Text itemName;
     public Image itemImage;
+    public Image selectIndicator;
 
     public void Assign(Item item, bool selected)
     {
         itemName.text = item?.Name ?? "";
+        if (item == null)
+        {
+            itemImage.gameObject.SetActive(false);
+        }
+        else
+        {
+            itemImage.gameObject.SetActive(true);
+            itemImage.sprite = Resources.Load<Sprite>(item.Url);
+        }
         ChangeActive(selected);
     }
 
     public void ChangeActive(bool selected)
     {
-        if (selected)
-        {
-            itemImage.color = Color.green;
-        }
-        else
-        {
-            itemImage.color = Color.white;
-        }
+        selectIndicator.gameObject.SetActive(selected);
     }
 
     public void Remove()
     {
         itemName.text = "";
+        itemImage.gameObject.SetActive(false);
     }
 }
